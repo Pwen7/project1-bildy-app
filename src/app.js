d64c81fd99d75926c3ba550ce8fea557790fdb0f
Wen <paulawen7@gmail.com>
2026-03-09 12:27:44 +0100

    Divide app & index

--- src/app.js
+++ src/app.js
@@ -1,5 +1,4 @@
 import express from 'express'
-import dbConnect from './config/db.js'
 import routes from './routes/index.js'
 import { errorHandler, notFound } from './middlewares/error.middleware.js'
 import { logger } from './middlewares/logger.js'
@@ -11,30 +10,14 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(logger)
 
+// Servir archivos estáticos
+app.use('/uploads', express.static('uploads'))
 
 // API routes
 app.use('/api', routes)
-
 
 // Manejo de errores
 app.use(notFound)
 app.use(errorHandler)
 
-
-// Iniciar servidor
-const PORT = process.env.PORT || 3000
-
-const startServer = async () => {
-    try {
-        await dbConnect()
-        app.listen(PORT, () => {
-            console.log(`🚀 Servidor en http://localhost:${PORT}`)
-            console.log(`📚 API en http://localhost:${PORT}/api`)
-        })
-    } catch (error) {
-        console.error('❌ Error al iniciar:', error)
-        process.exit(1)
-    }
-}
-
-startServer()
+export default app
---
+++ src/index.js
@@ -0,0 +1,20 @@
+// import 'dotenv/config'
+import app from './app.js'
+import dbConnect from './config/db.js'
+
+const PORT = process.env.PORT || 3000
+
+const startServer = async () => {
+    try {
+        await dbConnect()
+        app.listen(PORT, () => {
+            console.log(`🚀 Servidor en http://localhost:${PORT}`)
+            console.log(`📚 API en http://localhost:${PORT}/api`)
+        })
+    } catch (error) {
+        console.error('❌ Error al iniciar:', error)
+        process.exit(1)
+    }
+}
+
+startServer()
