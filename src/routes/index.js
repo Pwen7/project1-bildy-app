09da31a353b517a0273a97d606a0f83ae44fc769
Wen <154251716+Pwen7@users.noreply.github.com>
2026-03-29 22:54:30 +0200

    routes: user.routes,js & index.js

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,5 +1,8 @@
-import { Router } from "express"
+import { Router } from 'express'
+import userRoutes from './user.routes.js'
 
 const router = Router()
+
+router.use('/api/user', userRoutes)
 
 export default router
