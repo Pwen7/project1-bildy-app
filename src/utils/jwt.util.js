e8b482189633ebc3412091d7664a5a8b71d5adff
Wen <154251716+Pwen7@users.noreply.github.com>
2026-04-29 11:58:20 +0200

    test first

--- src/utils/jwt.util.js
+++ src/utils/jwt.util.js
@@ -1,17 +1,15 @@
 import jwt from 'jsonwebtoken'
 import crypto from 'node:crypto'
 
-const JWT_SECRET = process.env.JWT_SECRET
-const JWT_EXPIRES_IN = process.env.JWT_EXPIRES_IN || '2h'
-// const JWT_REFRESH_EXPIRES_IN = process.env.JWT_REFRESH_EXPIRES_IN || '7d'
-
 export const generateAccessToken = (user) => {
+    const secret = process.env.JWT_SECRET
+    const expiresIn = process.env.JWT_EXPIRES_IN || '2h'
     return jwt.sign({
         _id: user._id,
         role: user.role
     },
-        JWT_SECRET,
-        { expiresIn: JWT_EXPIRES_IN }
+        secret,
+        { expiresIn }
     )
 }
 
@@ -28,5 +26,5 @@ export const generateRefreshToken = () => {
 // }
 
 export const verifyAccessToken = (token) => {
-    return jwt.verify(token, JWT_SECRET)
+    return jwt.verify(token, process.env.JWT_SECRET)
 }
